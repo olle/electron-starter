664f433e5c5ede9a2767fda3080f73a924a910ba
Olle Törnström <olle@studiomediatech.com>
2022-04-07 22:35:37 +0200

    Prepare version 1.0.0
    * Good enough as a starter template, works a little, and has
      no audit failures. This is ok.

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
-(function() {
-  'use strict';
+(function () {
+  "use strict";
 
-  // For our Java app, we want to spawn it and run in background
-  var spawn = require('child_process').spawn;
-  var fs = require('fs');
-  var out = fs.openSync('./out.log', 'a');
-  var err = fs.openSync('./out.log', 'a');
-  var child = spawn('java', ['-jar', __dirname + '/target/app.jar'], {
-    detached: true,
-    stdio: ['ignore', out, err]
-  });
+  // During development we direct STDOUT/STDERR from the backend app to a file
+  var fs = require("fs");
+  var out = fs.openSync("./out.log", "a");
+  var err = fs.openSync("./out.log", "a");
+
+  // The backend Java App is spawned to run in the background
+  var spawn = require("child_process").spawn;
+  var child = spawn(
+    "java",
+    ["-Dserver.port=8181", "-jar", __dirname + "/target/app.jar"],
+    {
+      detached: true,
+      stdio: ["ignore", out, err],
+    }
+  );
   child.unref();
 
-  var electron = require('electron');
+  var electron = require("electron");
   var app = electron.app; // Module to control application life.
   var BrowserWindow = electron.BrowserWindow; // Module to create native browser window.
 
@@ -21,31 +27,31 @@
   var mainWindow;
 
   // Quit when all windows are closed.
-  app.on('window-all-closed', function() {
+  app.on("window-all-closed", function () {
     // On OS X it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform != 'darwin') {
+    if (process.platform != "darwin") {
       app.quit();
     }
   });
 
   // This method will be called when Electron has finished
   // initialization and is ready to create browser windows.
-  app.on('ready', function() {
+  app.on("ready", function () {
     // Create the browser window.
     mainWindow = new BrowserWindow({
       width: 1000,
-      height: 700
+      height: 700,
     });
 
     // and load the index.html of the app.
-    mainWindow.loadURL('file://' + __dirname + '/index.html');
+    mainWindow.loadURL("file://" + __dirname + "/index.html?port=8181");
 
-    // Open the DevTools.
+    // Open the DevTools
     //mainWindow.webContents.openDevTools();
 
     // Emitted when the window is closed.
-    mainWindow.on('closed', function() {
+    mainWindow.on("closed", function () {
       // Dereference the window object, usually you would store windows
       // in an array if your app supports multi windows, this is the time
       // when you should delete the corresponding element.
@@ -53,9 +59,8 @@
     });
   });
 
-  app.on('quit', function() {
-    // Close and destroy the JAVA process
-    child.kill('SIGINT');
+  app.on("quit", function () {
+    // Close and destroy the backend Java process
+    child.kill("SIGINT");
   });
-
 })();
